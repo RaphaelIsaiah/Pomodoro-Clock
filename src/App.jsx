9042c2ca640eb3c0de530d0232e2c8a8e0365f4c
RaphaelIsaiah <rafvie12@gmail.com>
2025-02-22 19:40:38 +0100

    feat: update favicon and Open Graph image; remove unused clock SVG

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Pomodoro from "./components/Pomodoro";
 function App() {
   return (
     <div
-      className="app bg-black min-h-screen text-center break-words px-5 flex flex-col gap-10 
+      className="app bg-black min-h-screen text-center break-words px-5 pt-10 flex flex-col gap-10
     items-center justify-center text-black text-2xl select-none"
     >
       <h1 className="heading text-4xl sm:text-5xl px-2 font-one font-semibold text-gold">
