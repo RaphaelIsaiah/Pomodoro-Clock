74430f58fd13b0eb4e48a7fc774ecde1135410f7
RaphaelIsaiah <rafvie12@gmail.com>
2025-02-22 19:47:38 +0100

    Changed placement of timerControl buttons for improved functionality

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -10,6 +10,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const TimerControls = ({ onStartStop, onReset, isRunning }) => {
   return (
     <div className="p-1 flex gap-4 justify-center ">
+      
+      <button
+        id="reset"
+        onClick={onReset}
+        className="bg-gold rounded-full w-2/5 p-2 transition-all duration-150 ease-in-out hover:bg-pewter hover:text-[1.1rem]
+         active:bg-pewter active:text-[1.1rem] shadow-4xl"
+      >
+        <FontAwesomeIcon icon={faRotateRight} />
+      </button>
+
       <button
         id="start_stop"
         onClick={onStartStop}
@@ -22,14 +32,6 @@ const TimerControls = ({ onStartStop, onReset, isRunning }) => {
         ) : (
           <FontAwesomeIcon icon={faPlay} />
         )}
-      </button>
-      <button
-        id="reset"
-        onClick={onReset}
-        className="bg-gold rounded-full w-2/5 p-2 transition-all duration-150 ease-in-out hover:bg-pewter hover:text-[1.1rem]
-         active:bg-pewter active:text-[1.1rem] shadow-4xl"
-      >
-        <FontAwesomeIcon icon={faRotateRight} />
       </button>
     </div>
   );
